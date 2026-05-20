149a0ee3298bbff2b5fe6e8127eae573685d8caa
jacobotoll <jacobo.toll@gmail.com>
2026-05-20 13:55:48 +0200

    Fix history browser CI checks

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ export default tseslint.config(
       "**/dist/**",
       "**/coverage/**",
       "**/target/**",
+      "apps/history-browser/t3code-history-browser/**",
       "node_modules/**"
     ]
   },
